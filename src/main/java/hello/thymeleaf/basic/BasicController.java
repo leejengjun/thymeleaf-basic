100d8146fd253c5206ba41877e88e0a2ef2f11b4
Jeongjun.lee <youngwoo4215@naver.com>
2022-10-18 10:27:16 +0900

    Initial commit

---
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -0,0 +1,9 @@
+package hello.thymeleaf.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+}
